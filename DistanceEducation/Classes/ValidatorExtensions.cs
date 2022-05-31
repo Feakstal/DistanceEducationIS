794f06bed91d3a5c9d4aea63ce1c3aa18ad80031
Feakstal <74710033+Feakstal@users.noreply.github.com>
2022-05-31 20:07:07 +0300

    Regex for textboxes, comboboxes and datapickers

---
+++ DistanceEducation/Classes/ValidatorExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Windows.Input;
+
+namespace DistanceEducation.Classes
+{
+    public static class ValidatorExtensions
+    {
+        public static void PreviewTextInput_ControlLetters(object sender, TextCompositionEventArgs e)
+        {
+            if (!Char.IsDigit(e.Text, 0)) e.Handled = true;
+        }
+
+        public static void PreviewTextInput_ControlForDate(object sender, TextCompositionEventArgs e)
+        {
+            if (!Char.IsDigit(e.Text, 0) && !(e.Text == ".") && !(e.Text == ":")) e.Handled = true;
+        }
+
+        public static void PreviewTextInput_ControlNumbers(object sender, TextCompositionEventArgs e)
+        {
+            var regex = new Regex(@"[абвгдеёжзийклмнопрстуфхцчшщьыъэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЬЫЪЭЮЯabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ]");
+            if (!regex.IsMatch(e.Text)) e.Handled = true;
+        }
+
+        public static void PreviewTextInput_ControlSpec(object sender, TextCompositionEventArgs e)
+        {
+            var regex = new Regex(@"[,.1234567890абвгдеёжзийклмнопрстуфхцчшщьыъэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЬЫЪЭЮЯabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ]");
+            if (!regex.IsMatch(e.Text)) e.Handled = true;
+        }
+    }
+}
